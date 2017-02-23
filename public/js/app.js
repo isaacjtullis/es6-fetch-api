cb33ff70c4bdad5a8b1e70ddbcbba4f7732b1cf7
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-23 11:21:45 -0700

    Used Fetch that utalized promises to retrieve and add resources.

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+let data = {
+  book: {
+    name: 'book from fetch'
+  }
+}
+let jsonStringData = JSON.stringify(data);
+
+fetch('http://localhost:4567/books.json', {
+  method: "post",
+  body: jsonStringData
+}).then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+      throw(error);
+    }
+  })
+  .then(response => response.json())
+  .then(body => {
+    console.log(body);
+  })
+  .catch(error => console.error(`Error in fetch: ${error.message}`));
