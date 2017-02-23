4e379e17b32290a43e48b3effcf9095dc02fbe57
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-23 12:38:29 -0700

    Appending the api information to the screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+/*
 let data = {
   book: {
     name: 'book from fetch'
@@ -20,5 +21,26 @@ fetch('http://localhost:4567/books.json', {
   .then(response => response.json())
   .then(body => {
     console.log(body);
+  })
+  .catch(error => console.error(`Error in fetch: ${error.message}`));
+*/
+
+//Used the above code to post to my book list
+
+fetch('http://localhost:4567/books.json')
+  .then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+      throw(error);
+    }
+  })
+  .then(response => response.json())
+  .then(body =>  {
+    body.books.forEach(function(book){
+      $('#books').append('<li>'+ book.name +'<li>');
+    })
   })
   .catch(error => console.error(`Error in fetch: ${error.message}`));
